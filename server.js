e3d8abb7c9d38ce1ef4ef77537ef25465e96e26a
Jasen Green <jgreen@xlanes.lcl>
2018-11-19 13:48:21 -0700

    Update Server

---
+++ server.js
@@ -0,0 +1,15 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.post("/", function(req, res) {
+  //add request
+});
+
+app.listen(3000, function() {
+  console.log("Node is Running on port 3000");
+});
