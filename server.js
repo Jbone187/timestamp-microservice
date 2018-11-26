0604827c7218cb584b6e7046302eb144aa3ffbda
Jasen Green <jgreen@xlanes.lcl>
2018-11-26 13:19:53 -0700

    Created New Branch

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const moment = require("moment");
 moment().format();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static("public"));
+//app.use(express.static("public"));
 
-app.post("/", function(req, res) {
-  //add request
+app.get("/", function(req, res) {
+  res.json(moment());
 });
 
 app.listen(3000, function() {
-  console.log("Node is Running on port 3000");
+  console.log("Node is Running Microservice");
 });
