a109ca09622f8bfa13860cb8bcabfe80c15ac8ac
Jasen Green <jasen.green@protonmail.com>
2018-12-05 11:47:22 -0700

    Update date function and added commits

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const app = express();
 const moment = require("moment");
 
 app.use(express.static("public"));
-
+//Unix and Natural date formation
 app.get("/:query", function(req, res) {
   const urlString = moment.unix(req.params.query);
   const validate = urlString.isValid();
   const naturalDate = moment(urlString).format("MMMM Do YYYY");
   const unixDate = moment(req.params.query, "MMMM D, YYYY").format("X");
-
+//Checking validation
   if (validate) {
     res.json({
       unix: req.params.query,
